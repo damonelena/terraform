b1fbb650216a40cc7999c3a50fa3d2846b2e5869
damonelena <81850212+damonelena@users.noreply.github.com>
2022-02-24 13:46:46 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,10 @@
-
-  provider "aws" {
-  region     = "us-west-2"
+provider "aws" {
+  region = "us-west-2"
+    access_key = "AKIARSN367ZMB35MQQUT"
+  secret_key = "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"
 }
 
 resource "aws_instance" "myec2" {
-   ami = "ami-0d6621c01e8c2de2c"
-   instance_type = "t2.micro"
-   count = 5
+  ami           = "ami-06cffe063efe892ad"
+  instance_type = "t2.nano"
 }
