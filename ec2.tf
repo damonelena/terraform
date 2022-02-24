92947adcd1b9e65c1a6a6e442a6cef26f93d8adc
damonelena <81850212+damonelena@users.noreply.github.com>
2022-02-24 13:50:34 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "us-west-2"
-    access_key = "AKIARSN367ZMB35MQQUT"
+  access_key = "AKIARSN367ZMB35MQQUT"
   secret_key = "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"
 }
 
